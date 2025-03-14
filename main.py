c3a559745a66c36c3190e0599aab9b7c36ba705e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-03-14 14:13:25 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Only the main python code here
+# main python code here
 
 # imports
 import pandas
@@ -9,28 +9,41 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     AdaBoostClassifier,
 )
+from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 
-mlp_pipeline = Pipeline(
+# MLP Classifier definition
+mlp_classifier = Pipeline(
     [
         ("scaler", StandardScaler()),
-        ("mlp", MLPClassifier(random_state=42, max_iter=1000, solver='sgd', hidden_layer_sizes=(100, 50), activation='tanh'))
+        (
+            "mlp",
+            MLPClassifier(
+                random_state=42,
+                max_iter=1000,
+                solver="sgd",
+                hidden_layer_sizes=(100, 50),
+                activation="tanh",
+            ),
+        ),
     ]
 )
 
-# training
+# reading data files
 trainFile = pandas.read_csv("treino.csv")
+testFile = pandas.read_csv("teste.csv")
 
+# training and testing data
 X = trainFile.drop("target", axis=1)
 y = trainFile["target"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# models
+# models declaration
 models = {
     "Optimized Random Forest": RandomForestClassifier(
         random_state=42, n_estimators=500, bootstrap=False
@@ -39,11 +52,11 @@ models = {
     "Gradient Boosting": GradientBoostingClassifier(random_state=42),
     "Ada Boost": AdaBoostClassifier(random_state=42),
     "Decision Tree": DecisionTreeClassifier(random_state=42),
-    "MLP Pipeline": mlp_pipeline,
+    "MLP Classifier": mlp_classifier,
 }
 
 
-# execution using default params
+# training each model and finding out each one's score
 def fit_and_score(models, X_train, y_train):
     model_scores = {}
     for name, model in models.items():
